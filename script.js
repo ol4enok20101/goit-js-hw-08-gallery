17c1ef85a8fcf0bf3742f9da03067dc1680b2974
Olga Uzun <67106495+ol4enok20101@users.noreply.github.com>
2021-01-11 18:14:48 +0200

    close on esc added

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
-import galleryImg from './gallery-items.js'
+import galleryImg from "./gallery-items.js";
 
-const gallery = document.querySelector('.js-gallery')
+const gallery = document.querySelector(".js-gallery");
 
 function createGalleryItem({ preview, original, description }) {
+  const galleryListItem = document.createElement("li");
+  galleryListItem.classList.add("gallery__item");
 
-    const galleryListItem = document.createElement('li');
-    galleryListItem.classList.add("gallery__item");
+  const galleryLink = document.createElement("a");
+  galleryLink.setAttribute("href", original);
+  galleryLink.classList.add("gallery__link");
+  galleryListItem.appendChild(galleryLink);
 
-    const galleryLink = document.createElement('a');
-    galleryLink.setAttribute('href', (original));
-    galleryLink.classList.add('gallery__link');
-    galleryListItem.appendChild(galleryLink);
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = preview;
+  galleryImage.alt = description;
+  galleryImage.dataset.source = original;
+  galleryLink.appendChild(galleryImage);
 
-    const galleryImage = document.createElement('img');
-    galleryImage.classList.add("gallery__image");
-    galleryImage.src = (preview);
-    galleryImage.alt = (description);
-    galleryImage.dataset.source = original;
-    galleryLink.appendChild(galleryImage);
-
-    return galleryListItem
+  return galleryListItem;
 }
 
 const arr = galleryImg.map(createGalleryItem);
-// console.log(arr)
-
 gallery.append(...arr);
 
-gallery.addEventListener('click', onClick);
-const lightbox = document.querySelector('.js-lightbox')
-function onClick(evt) {
+gallery.addEventListener("click", openModal);
+const lightbox = document.querySelector(".js-lightbox");
 
-    evt.preventDefault();
-    console.log(evt.target.dataset.source)
-
-    if (evt.target.nodeName !== 'IMG') {
-        return
-    } lightbox.classList.add('is-open');
+function openModal(evt) {
+  evt.preventDefault();
+  modalImage.src = evt.target.dataset.source;
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  lightbox.classList.add("is-open");
 }
 
-const closeButton = document.querySelector('.lightbox__button');
-closeButton.addEventListener('click', closeClick);
+const closeButton = document.querySelector(".lightbox__button");
+closeButton.addEventListener("click", closeModal);
 
-function closeClick(close) {
-    lightbox.classList.remove('is-open');
-    lightboxImage.src = '';
+window.addEventListener("keydown", escapeClose);
+
+function escapeClose(event) {
+  if (event.code === "Escape") {
+    closeModal();
+  }
 }
 
-const lightboxImage = document.querySelector('.lightbox__image');
+const closeOverlay = document.querySelector(".lightbox__overlay");
+closeOverlay.addEventListener("click", closeModal);
 
-if (lightbox.classList.contains('is-open')) {
-    lightboxImage.src = (arr.original);
+function closeModal(close) {
+  lightbox.classList.remove("is-open");
+  modalImage.src = "";
+}
+
+const modalImage = document.querySelector(".lightbox__image");
+
+if (lightbox.classList.contains("is-open")) {
+  modalImage.src = arr.original;
 }
